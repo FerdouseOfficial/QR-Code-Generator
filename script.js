fc09d0591eea20f6c149fd218a9c849a790d3d17
FerdouseOfficial <mollajihad402@gmail.com>
2025-10-16 15:08:48 +0600

    Almos there

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const input = document.getElementById('userInput');
-const button = document.getElementById('submit');
+const input = document.getElementById("userInput");
+const button = document.getElementById("submit");
+const ImgBox = document.getElementById("imgBox");
+const QRImage = document.getElementById("qrImage");
 
-async function mainWork(Input) {
-    const url = fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${Input}` )
+async function generateQR(Input) {
+  const url = await fetch(
+    `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${Input}`
+  );
 }
 
-button.addEventListener(('click'), () => {
-    const data = input.value;
-    console.log(data)
-})
+button.addEventListener("click", () => {
+  const data = input.value;
+  QRImage.src = generateQR(data)
+});
